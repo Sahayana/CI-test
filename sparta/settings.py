d1060e6b4bbff5c25067e43b6f5ff2aad19f9129
sahayana <saokhueyang@gmail.com>
2022-02-13 16:11:18 +0900

    MySQL set up

--- sparta/settings.py
+++ sparta/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from typing import List
 
+import pymysql
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,11 +75,16 @@ WSGI_APPLICATION = "sparta.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+pymysql.install_as_MySQLdb()
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "spartaDB",
+        "USER": "root",
+        "PASSWORD": "rmatjd123",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
